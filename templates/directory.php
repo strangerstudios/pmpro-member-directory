5bce84e112a5e5c0539b6597ae1949b3fae7520a
Theuns Coetzee <ipokkel@gmail.com>
2021-09-23 10:04:35 +0200

    apply WPCS spacing

--- templates/directory.php
+++ templates/directory.php
@@ -302,7 +302,7 @@ $sqlQuery = $sql_parts['SELECT'] . $sql_parts['JOIN'] . $sql_parts['WHERE'] . $s
 							<?php } ?>
 							<?php
 							//filter the fields
-							$fields_array = apply_filters('pmpro_member_directory_fields', $fields_array, $auser);
+							$fields_array = apply_filters( 'pmpro_member_directory_fields', $fields_array, $auser );
 
 							if(!empty($fields_array))
 							{
