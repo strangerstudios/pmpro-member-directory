b8dcd0d3e69a6b8c43c590cad82e9aafab786dea
Jason Coleman <33220397+ideadude@users.noreply.github.com>
2022-03-18 09:18:15 -0400

    Update templates/directory.php

--- templates/directory.php
+++ templates/directory.php
@@ -312,7 +312,7 @@ $sqlQuery = $sql_parts['SELECT'] . $sql_parts['JOIN'] . $sql_parts['WHERE'] . $s
 					{
 						$auser = get_userdata($auser->ID);
 						$auser->membership_level = pmpro_getMembershipLevelForUser($auser->ID);
-						$user_identifier = pmpro_md_user_identifier();
+						$user_identifier = pmpromd_user_identifier();
 						$count++;
 						?>
 						<tr id="pmpro_member_directory_row-<?php echo $auser->ID; ?>" class="pmpro_member_directory_row<?php if(!empty($link) && !empty($profile_url)) { echo " pmpro_member_directory_linked"; } ?>">
