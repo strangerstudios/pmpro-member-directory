df16b98b3419dc1c56c4eadf77de88df2b9d40da
Andrew Lima <andrew@arctek.co.za>
2024-02-16 11:44:19 +0200

    Improve security by restricting shortcode attributes
    * Improved security by only allowing certain characters within field attributes.

--- templates/directory.php
+++ templates/directory.php
@@ -53,6 +53,7 @@ function pmpromd_shortcode($atts, $content=null, $code="")
 		$levels = implode( ',', $levels );
 	}
 
+
 	if($show_avatar === "0" || $show_avatar === "false" || $show_avatar === "no"  || $show_avatar === false)
 		$show_avatar = false;
 	else
@@ -83,6 +84,11 @@ function pmpromd_shortcode($atts, $content=null, $code="")
 	else
 		$s = "";
 
+	// Set the default order value to be either ASC or DESC.
+	if ( $order !== 'DESC' ) {
+		$order = 'ASC';
+	}
+
 	if(isset($_REQUEST['pn']))
 		$pn = intval($_REQUEST['pn']);
 	else
@@ -107,6 +113,8 @@ $sql_parts['WHERE'] = "WHERE mu.status = 'active' AND (umh.meta_value IS NULL OR
 
 $sql_parts['GROUP'] = "GROUP BY u.ID ";
 
+// Clean up order_by to only include text, underscores and periods.
+$order_by = preg_replace( '/[^a-z._]/', '', $order_by );
 $sql_parts['ORDER'] = "ORDER BY ". esc_sql($order_by) . " " . $order . " ";
 
 $sql_parts['LIMIT'] = "LIMIT $start, $limit";
@@ -136,6 +144,7 @@ if( $s ) {
 
 // If levels are passed in.
 if ( $levels ) {
+	$levels = preg_replace('/[^0-9,]/', '', $levels ); // Only allow commas and numeric values.
 	$sql_parts['WHERE'] .= "AND mu.membership_id IN(" . esc_sql($levels) . ") ";
 }
 
