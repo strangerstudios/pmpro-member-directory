3573c88f1016c299b8aeb479c0eeef069f77e3eb
Kim Coleman <kim@strangerstudios.com>
2024-11-17 13:30:58 -0500

    Fixing search form not to wrap in pmpro class unless used as standalone shortcode

--- shortcodes/search.php
+++ shortcodes/search.php
@@ -3,6 +3,15 @@
  * Show the content for the [pmpro_member_directory_search] shortcode.
  */
 function pmpromd_search_shortcode() {
-	echo pmpromd_search_form();
+	ob_start();
+	?>
+	<div class="<?php echo esc_attr( pmpro_get_element_class( 'pmpro' ) ); ?>">
+		<?php pmpromd_search_form(); ?>
+	</div>
+	<?php
+	$content = ob_get_contents();
+	ob_end_clean();
+
+	return $content;
 }
 add_shortcode( 'pmpro_member_directory_search', 'pmpromd_search_shortcode' );
