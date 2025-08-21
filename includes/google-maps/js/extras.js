6b6633fc12ddfb97c1fc9d9fb9185a17e82ef741
Andrew Lima <andrew@arctek.co.za>
2025-08-21 17:10:49 +0200

    Fix spacing.

--- includes/google-maps/js/extras.js
+++ includes/google-maps/js/extras.js
@@ -21,17 +21,17 @@ jQuery(document).ready(function() {
 		});
 	}
 
-    // Initially hide the address fields if the checkbox is not checked
-    if (!jQuery('#pmpromd_map_optin').is(':checked')) {
+	// Initially hide the address fields if the checkbox is not checked
+	if (!jQuery('#pmpromd_map_optin').is(':checked')) {
 		jQuery('.pmpromd-map-address-field').closest('div').hide();
-    }
+	}
 
-    // Toggle visibility when the checkbox state changes
-    jQuery('#pmpromd_map_optin').on('change', function() {
-        if (jQuery(this).is(':checked')) {
+	// Toggle visibility when the checkbox state changes
+	jQuery('#pmpromd_map_optin').on('change', function() {
+		if (jQuery(this).is(':checked')) {
 			jQuery('.pmpromd-map-address-field').closest('div').show();
-        } else {
+		} else {
 			jQuery('.pmpromd-map-address-field').closest('div').hide();
-        }
-    });
+		}
+	});
 });
