c68da34cc92f8388fc72400b745a8c7a69617103
Andrew Lima <andrew@arctek.co.za>
2025-08-21 17:10:23 +0200

    Remove redundant classes.

--- includes/google-maps/js/extras.js
+++ includes/google-maps/js/extras.js
@@ -5,17 +5,17 @@ jQuery(document).ready(function() {
 		
 		if ( jQuery('#pmpromd_hide_directory').is(':checked') ) {
 			jQuery('#pmpromd_map_optin').closest('div').hide();
-			jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').hide();
+			jQuery('.pmpromd-map-address-field').closest('div').hide();
 		}
 
 		jQuery('#pmpromd_hide_directory').on('change', function() { 
 			if ( jQuery(this).is(':checked') ) {
 					jQuery('#pmpromd_map_optin').closest('div').hide();
-					jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').hide();
+					jQuery('.pmpromd-map-address-field').closest('div').hide();
 			} else {
 				jQuery('#pmpromd_map_optin').closest('div').show();
 				if( jQuery('#pmpromd_map_optin').is(':checked') ) {
-					jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').show();
+					jQuery('.pmpromd-map-address-field').closest('div').show();
 				}
 			}
 		});
@@ -23,15 +23,15 @@ jQuery(document).ready(function() {
 
     // Initially hide the address fields if the checkbox is not checked
     if (!jQuery('#pmpromd_map_optin').is(':checked')) {
-		jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').hide();
+		jQuery('.pmpromd-map-address-field').closest('div').hide();
     }
 
     // Toggle visibility when the checkbox state changes
     jQuery('#pmpromd_map_optin').on('change', function() {
         if (jQuery(this).is(':checked')) {
-			jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').show();
+			jQuery('.pmpromd-map-address-field').closest('div').show();
         } else {
-			jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').hide();
+			jQuery('.pmpromd-map-address-field').closest('div').hide();
         }
     });
 });
