f4da4268b7ca283ccc7c05f1c779d56c8358c710
Andrew Lima <andrew@arctek.co.za>
2025-08-21 17:09:12 +0200

    Fix error copilot commits.

--- includes/google-maps/js/extras.js
+++ includes/google-maps/js/extras.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function() {
-
+	
 	// Only show the #pmpromd_map_optin if the pmpromd_hide_directory is unchecked.
 	if ( jQuery('#pmpromd_hide_directory').length ) {
-	
+		
 		if ( jQuery('#pmpromd_hide_directory').is(':checked') ) {
 			jQuery('#pmpromd_map_optin').closest('div').hide();
 			jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').hide();
@@ -15,33 +15,23 @@ jQuery(document).ready(function() {
 			} else {
 				jQuery('#pmpromd_map_optin').closest('div').show();
 				if( jQuery('#pmpromd_map_optin').is(':checked') ) {
-			jQuery('.pmpromd-map-address-field').closest('div').hide();
-		}
-
-		jQuery('#pmpromd_hide_directory').on('change', function() { 
-			if ( jQuery(this).is(':checked') ) {
-					jQuery('#pmpromd_map_optin').closest('div').hide();
-					jQuery('.pmpromd-map-address-field').closest('div').hide();
-			} else {
-				jQuery('#pmpromd_map_optin').closest('div').show();
-				if( jQuery('#pmpromd_map_optin').is(':checked') ) {
-					jQuery('.pmpromd-map-address-field').closest('div').show();
+					jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').show();
 				}
 			}
 		});
 	}
 
-	// Initially hide the address fields if the checkbox is not checked
-	if (!jQuery('#pmpromd_map_optin').is(':checked')) {
+    // Initially hide the address fields if the checkbox is not checked
+    if (!jQuery('#pmpromd_map_optin').is(':checked')) {
 		jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').hide();
-	}
+    }
 
-	// Toggle visibility when the checkbox state changes
-	jQuery('#pmpromd_map_optin').on('change', function() {
-		if (jQuery(this).is(':checked')) {
+    // Toggle visibility when the checkbox state changes
+    jQuery('#pmpromd_map_optin').on('change', function() {
+        if (jQuery(this).is(':checked')) {
 			jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').show();
-		} else {
+        } else {
 			jQuery('.pmpromd-map-address-field, .pmpromd-map-address-field').closest('div').hide();
-		}
-	});
+        }
+    });
 });
